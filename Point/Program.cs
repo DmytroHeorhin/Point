75a59a950029dbf9790076448e2df0d499215633
Heorhin, Dmytro <Dmytro.Heorhin@wolterskluwer.com>
2017-05-23 10:46:33 +0300

    init

---
+++ Point/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Point
+{
+    public struct Point
+    {
+        public int X { get; }
+
+        public Point(int x)
+        {
+            X = x;
+        }
+
+        public Point IncX()
+        {
+            return new Point(X + 1);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var points = new List<Point>(Enumerable.Range(1, 10).Select(p => new Point(p)));
+            for (var i = 0; i < points.Count; i++)
+            {
+                points[i] = points[i].IncX();
+            }
+            foreach (var p in points)
+            {
+                Console.WriteLine(p.X);
+            }
+            Console.ReadKey();
+        }
+    }
+}
